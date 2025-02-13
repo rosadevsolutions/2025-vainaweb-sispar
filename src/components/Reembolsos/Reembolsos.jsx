dc38b03372f3d13e4736b75ee90396940acbd68d
Romulo Guilherme da Silva Rosa <rosadevsolutions@gmail.com>
2025-02-13 11:00:57 -0300

    component reembolsos

---
+++ src/components/Reembolsos/Reembolsos.jsx
@@ -0,0 +1,12 @@
+import Breadcrumb from "../Breadcrumb/Breadcrumb"
+
+export default function Reembolsos() {
+  return (
+    <div>
+      <Breadcrumb actualpage="Reembolsos" />
+      <section>
+        Component Reembolsos
+      </section>
+    </div>
+  )
+}
