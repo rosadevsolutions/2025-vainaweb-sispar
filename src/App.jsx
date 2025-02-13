956ac4791b0cf8add8a632bdac7629843ccdeb16
Romulo Guilherme da Silva Rosa <rosadevsolutions@gmail.com>
2025-02-13 11:03:44 -0300

    App com rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Login from "./components/Login/Login"
+import Reembolsos from "./components/Reembolsos/Reembolsos"
+import Solicitacao from "./components/Solicitacao/Solicitacao"
+import "./global.scss"
+
 export default function App() {
   return (
-    <>
-      <h1>SISPAR</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <Login /> } />
+        <Route path="/reembolsos" element={ <Reembolsos /> } />
+        <Route path="/solicitacao" element={ <Solicitacao /> } />
+      </Routes>
+    </BrowserRouter>
   )
 }
