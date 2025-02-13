cd8bfc5729f4eb4ea7be08eb74858f811ccd574d
Romulo Guilherme da Silva Rosa <rosadevsolutions@gmail.com>
2025-02-13 12:45:45 -0300

    component solicitacao com breadcrumb

--- src/components/Solicitacao/Solicitacao.jsx
+++ src/components/Solicitacao/Solicitacao.jsx
@@ -1,6 +1,9 @@
+import Breadcrumb from "../Breadcrumb/Breadcrumb"
+
 export default function Solicitacao() {
   return (
     <div>
+      <Breadcrumb page="Reembolsos" activePage="Solicitação de Reembolso" />
       <section>
         Component Solicitação
       </section>
