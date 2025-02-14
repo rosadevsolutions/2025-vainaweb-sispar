0dccb7878b64c371fefd4f245739c5c375e2342b
Romulo Guilherme da Silva Rosa <rosadevsolutions@gmail.com>
2025-02-13 21:10:09 -0300

    Solicitação com Braedcrumb

--- src/components/Solicitacao/Solicitacao.jsx
+++ src/components/Solicitacao/Solicitacao.jsx
@@ -3,7 +3,7 @@ import Breadcrumb from "../Breadcrumb/Breadcrumb"
 export default function Solicitacao() {
   return (
     <div>
-      <Breadcrumb page="Reembolsos" activePage="Solicitação de Reembolso" />
+      <Breadcrumb page="Reembolsos" activePage={`Solicitação de Reembolso`} />
       <section>
         Component Solicitação
       </section>
