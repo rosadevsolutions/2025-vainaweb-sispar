73b0b28ecd1676d94d177748c8895e6d6561ba2f
Romulo Guilherme da Silva Rosa <rosadevsolutions@gmail.com>
2025-02-14 02:03:22 -0300

    trocando cores e tamanhos por variaveis

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -6,6 +6,7 @@ import LoginButton from "../LoginButton/LoginButton"
 import LoginButtonText from "../LoginButtonText/LoginButtonText"
 import styles from "./Login.module.scss"
 
+
 export default function Login(){
 
   const navigate = useNavigate()
