d3879275b33947fee0c1a6509f5774844d3855fa
Romulo Guilherme da Silva Rosa <rosadevsolutions@gmail.com>
2025-02-17 12:25:52 -0300

    assets reembolsos icones

--- src/components/Reembolsos/Reembolsos.jsx
+++ src/components/Reembolsos/Reembolsos.jsx
@@ -1,12 +1,67 @@
 import Breadcrumb from "../Breadcrumb/Breadcrumb"
+import styles from "./Reembolsos.module.scss"
 
 export default function Reembolsos() {
   return (
     <div>
       <Breadcrumb activePage={`Reembolsos`} />
-      <section>
-        Component Reembolsos
-      </section>
+
+      <main className={styles.main__wrapper}>
+        <div className={styles.content__wrapper}>
+          <section className={styles.infos__wrapper}>
+            <h1 className={styles.infos__title}>Sistema de Reembolso</h1>
+            <p className={styles.infos__text}>Solicite novos pedidos de reembolso, visualize solicitações em análise e todo o histórico.</p>
+          </section>
+
+          <section className={styles.services__wrapper}>
+            <article className={styles.services__card}>
+              <img className={styles.services__icon} alt="ícone solicitar reembolso." />
+              <h3 className={styles.services__title}>Texto</h3>
+            </article>
+
+            <article className={styles.services__card}>
+              <img className={styles.services__icon} alt="ícone solicitar reembolso." />
+              <h3 className={styles.services__title}>Texto</h3>
+            </article>
+
+            <article className={styles.services__card}>
+              <img className={styles.services__icon} alt="ícone solicitar reembolso." />
+              <h3 className={styles.services__title}>Texto</h3>
+            </article>
+          </section>
+
+          <section className={styles.report__wrapper}>
+            <articles className={styles.report__card}>
+              <img className={styles.report__icon} alt="ícone solicitar reembolso." />
+              <p className={styles.report__metrics}>99</p>
+              <p className={styles.report__info}>Info</p>
+            </articles>
+
+            <articles className={styles.report__card}>
+              <img className={styles.report__icon} alt="ícone solicitar reembolso." />
+              <p className={styles.report__metrics}>99</p>
+              <p className={styles.report__info}>Info</p>
+            </articles>
+
+            <articles className={styles.report__card}>
+              <img className={styles.report__icon}  alt="ícone solicitar reembolso." />
+              <p className={styles.report__metrics}>99</p>
+              <p className={styles.report__info}>Info</p>
+            </articles>
+
+            <articles className={styles.report__card}>
+              <img className={styles.report__icon}  alt="ícone solicitar reembolso." />
+              <p className={styles.report__metrics}>99</p>
+              <p className={styles.report__info}>Info</p>
+            </articles>
+          </section>
+
+          <section className="status__wrapper">
+            <img className="status__icon" alt="ícone solicitar reembolso." />
+            <p className="status__info">Sistema atualizado.</p>
+          </section>
+        </div>
+      </main>
     </div>
   )
 }
