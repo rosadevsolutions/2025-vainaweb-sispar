5d01601c32bda4fdbcb58f60783c099ca9c2de94
Romulo Guilherme da Silva Rosa <rosadevsolutions@gmail.com>
2025-02-17 13:40:15 -0300

    reembolsos com icones

--- src/components/Reembolsos/Reembolsos.jsx
+++ src/components/Reembolsos/Reembolsos.jsx
@@ -1,3 +1,11 @@
+import IconAnalise from "../../assets/Reembolsos/icon-analise.svg"
+import IconAprovados from "../../assets/Reembolsos/icon-aprovados.svg"
+import IconHistorico from "../../assets/Reembolsos/icon-historico.svg"
+import IconRejeitados from "../../assets/Reembolsos/icon-rejeitados.svg"
+import IconSolicitados from "../../assets/Reembolsos/icon-solicitados.svg"
+import IconSolicitarReembolso from "../../assets/Reembolsos/icon-solicitar.svg"
+import IconStatus from "../../assets/Reembolsos/icon-status.svg"
+import IconVerificarAnalises from "../../assets/Reembolsos/icon-verificar.svg"
 import Breadcrumb from "../Breadcrumb/Breadcrumb"
 import styles from "./Reembolsos.module.scss"
 
@@ -18,17 +26,17 @@ export default function Reembolsos() {
 
           <section className={styles.services__wrapper}>
             <articles className={styles.services__card}>
-              <img className={styles.services__icon} alt="ícone solicitar reembolso" title="Solicitar reembolso" />
+              <img src={IconSolicitarReembolso} className={styles.services__icon} alt="ícone solicitar reembolso" title="Solicitar reembolso" />
               <h3 className={styles.report__title}>Solicitar Reembolso</h3>
             </articles>
 
             <articles className={styles.services__card}>
-              <img className={styles.services__icon} alt="ícone verificar análises" title="Verificar análises" />
+              <img src={IconHistorico} className={styles.services__icon} alt="ícone verificar análises" title="Verificar análises" />
               <h3 className={styles.report__title}>Verificar Análises</h3>
             </articles>
 
             <articles className={styles.services__card}>
-              <img className={styles.services__icon} alt="ícone de histórico"  title="Histórico" />
+              <img src={IconVerificarAnalises} className={styles.services__icon} alt="ícone de histórico"  title="Histórico" />
               <h3 className={styles.report__title}>Histórico</h3>
             </articles>
           </section>
@@ -36,33 +44,41 @@ export default function Reembolsos() {
 
           <section className={styles.report__wrapper}>
             <articles className={styles.report__card}>
-              <img className={styles.report__icon} alt="ícone solitados" title="Solicitados" />
-              <p className={styles.report__metrics}>182</p>
+              <div className={styles.report__icon__wrapper}>
+                <img src={IconSolicitados} className={styles.report__icon} alt="ícone solitados" title="Solicitados" />
+              </div>
+              <p className={styles.report__value}>182</p>
               <p className={styles.report__info}>Solicitados</p>
             </articles>
 
             <articles className={styles.report__card}>
-              <img className={styles.report__icon} alt="ícone em análise" title="Em análise" />
-              <p className={styles.report__metrics}>74</p>
+              <div className={styles.report__icon__wrapper}>
+                <img src={IconAnalise} className={styles.report__icon} alt="ícone em análise" title="Em análise" />
+              </div>
+              <p className={styles.report__value}>74</p>
               <p className={styles.report__info}>Em análise</p>
             </articles>
 
             <articles className={styles.report__card}>
-              <img className={styles.report__icon} alt="ícone aprovados" title="Aprovados" />
-              <p className={styles.report__metrics}>195</p>
+              <div className={styles.report__icon__wrapper}>
+                <img src={IconAprovados} className={styles.report__icon} alt="ícone aprovados" title="Aprovados" />
+              </div>
+              <p className={styles.report__value}>195</p>
               <p className={styles.report__info}>Aprovados</p>
             </articles>
 
             <articles className={styles.report__card}>
-              <img className={styles.report__icon} alt="ícone rejeitados" title="Rejeitados" />
-              <p className={styles.report__metrics}>41</p>
+              <div className={styles.report__icon__wrapper}>
+                <img src={IconRejeitados} className={styles.report__icon} alt="ícone rejeitados" title="Rejeitados" />
+              </div>
+              <p className={styles.report__value}>41</p>
               <p className={styles.report__info}>Rejeitados</p>
             </articles>
           </section>
 
           <section className="status__wrapper">
-            <img className="status__icon" alt="ícone sistema atualizado" title="Sistema atualizado" />
-            <p className="status__info">Sistema atualizado</p>
+            <img src={IconStatus} className="status__icon" alt="ícone sistema atualizado" title="Sistema atualizado" />
+            <p className="status__info">Sistema atualizado.</p>
           </section>
 
         </main>
